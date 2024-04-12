c766bad4c9c8f132bef8146d25495fbf358c281b
Violet Iapalucci <violet_iapalucci@outlook.com>
2024-04-12 10:16:53 -0400

    Add community warning

--- src/bootstrap.rs
+++ src/bootstrap.rs
@@ -1,8 +1,13 @@
+use std::io::Write as _;
+
+use colored::Colorize as _;
 use darling_api as darling;
 
 pub static PACKAGE_MANAGER: Darling = Darling;
 
 pub struct Darling;
+
+const OFFICIAL_MODULES: &[&str] = &["arch", "module", "npm", "vscode"];
 
 impl darling::PackageManager for Darling {
     fn name(&self) -> String {
@@ -10,6 +15,21 @@ impl darling::PackageManager for Darling {
     }
 
     fn install(&self, context: &darling::Context, package: &darling::InstallationEntry) -> anyhow::Result<()> {
+        // Warn that it's community made
+        if !OFFICIAL_MODULES.contains(&package.name.as_str()) {
+            println!(
+                "{} module {} is an unofficial community-made module which may contain malicious or unsafe code.",
+                "Warning:".yellow().bold(),
+                package.name.cyan().bold()
+            );
+            print!("Continue? (Y/n): ");
+            std::io::stdout().flush()?;
+            let mut buffer = String::new();
+            std::io::stdin().read_line(&mut buffer)?;
+            if buffer.trim().to_lowercase() != "y" {
+                anyhow::bail!("Cancelled installation due to unofficial package.");
+            }
+        }
         // Add the crate dependency
         std::process::Command::new("cargo")
             .arg("add")
@@ -54,7 +74,7 @@ impl darling::PackageManager for Darling {
         // Write to the module include! file
         let module_file_lines = std::fs::read_to_string(context.config.source_location.clone() + "/src/modules.rs")?
             .lines()
-            .filter(|line| line != &format!("darling_{}::PACKAGE_MANAGER,", &package.name))
+            .filter(|line| line.trim() != format!("&darling_{}::PACKAGE_MANAGER,", &package.name))
             .collect::<Vec<_>>()
             .join("\n");
         std::fs::write(context.config.source_location.clone() + "/src/modules.rs", module_file_lines)?;
